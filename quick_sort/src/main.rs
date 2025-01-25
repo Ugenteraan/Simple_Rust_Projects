9be8e9553efa8571bdded1f0e5f41cf01e749bbc
Ugenteraan Manogaran <m.ugenteraan15@gmail.com>
2025-01-25 18:28:07 +0800

    quick sort

--- quick_sort/src/main.rs
+++ quick_sort/src/main.rs
@@ -1,3 +1,85 @@
+// use rand::Rng;
+// use std::env;
+
+// fn quicksort(mut x: Vec<i32>, mut start: usize, pivot:usize) -> Vec<i32> {
+
+//     if pivot <= 1 || start == pivot{
+//         return x;
+//     }
+
+//     let mut swap_ptr = start;
+//     for curr_index in start..=pivot {
+
+//         if x[curr_index] > x[pivot] {
+//             continue;
+//         }else{
+//             if x[curr_index] < x[swap_ptr]{
+//                 x.swap(curr_index, swap_ptr);
+//                 swap_ptr += 1;
+//             }
+
+//             if curr_index == pivot {
+//                 swap_ptr += 1;
+//                 x.swap(curr_index, swap_ptr);
+//             }
+
+//         }
+
+//     }
+
+//     let left_half = quicksort(x, 0 as usize, swap_ptr);
+//     quicksort(left_half, swap_ptr, pivot)
+
+// }
+
+fn quicksort(x: &mut [i32]) -> () {
+
+    if x.len() <= 1 {
+        return ;
+    }
+
+    let pivot_index = partition(x); //we can pass x directly here since it's already a &mut.
+
+    quicksort(&mut x[..pivot_index]); //here, we need to specify &mut because x[..] is slicing it and it's non mutable by default.
+    quicksort(&mut x[pivot_index+1..]);
+
+}
+
+
+
+fn partition(x: &mut [i32]) -> usize{
+
+
+
+    let pivot_index: usize = x.len() - 1;
+    let pivot_element: i32 = x[pivot_index];
+
+    let mut i: usize = 0;
+
+    for j in 0..=pivot_index {
+
+        if x[j] < pivot_element {
+            x.swap(j, i);
+            i += 1;
+        }
+    }
+
+    x.swap(i, pivot_index);
+
+    return i;
+}
+
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+
+    let mut x: Vec<i32> = vec![8,2,5,0,1,8,7,6,9,4];
+
+
+    quicksort(&mut x);
+
+    println!("{:?}", x);
 }
